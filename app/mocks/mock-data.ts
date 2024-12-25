ef9bbf0d8d2a8c33bb25730f1f5b737aa9a0f39f
Dayvson Silva <userddssilva@gmail.com>
2024-12-24 23:23:41 -0400

    chore: unconnect backend

---
+++ app/mocks/mock-data.ts
@@ -0,0 +1,30 @@
+// Account data
+export const mockAccount: Account = {
+    id: "SDF22-23FLSDF-23LJFS",
+    availableBalance: 89.00,
+    currentBalance: 22.0,
+    officialName: "XXDDS",
+    mask: "MAO",
+    institutionId: "SD22-FSLK-213F",
+    name: "ABC",
+    type: "X",
+    subtype: "XY",
+    appwriteItemId: "OW22-L42J-FKL4-0FLSKJ",
+    shareableId: "FSOI22-23LKJ42-LKJ3KL4J-2LKJ3LK",
+  }
+
+export const mockAccounts: Account[] = [
+  mockAccount,
+  mockAccount,
+  mockAccount,
+  mockAccount
+];
+
+export const mockRequestAccounts = {
+  id: "U123-0K2FG",
+  data: mockAccounts
+}
+
+
+// Transaction data
+  
